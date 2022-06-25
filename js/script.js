86e9f707b1b51179cad1be6f67a6f4dcdbc30a99
Anastasiia <guba.anastaysha@gmail.com>
2022-06-25 22:59:58 +0300

    Added slider

---
+++ js/script.js
@@ -0,0 +1,34 @@
+new Swiper('.reviews__slider', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+        dynamicMainBullets: 1,
+    },
+
+    touchRatio: 2,
+    grabCursor: true,
+    spaceBetween: 20,
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+
+    loop: true,
+
+    autoplay: {
+        delay: 2000,
+        stopOnLastSlide: true,
+    },
+
+    speed: 700,
+
+    effect: 'coverflow',
+    coverflowEffect: {
+    rotate: 30,
+    slideShadows: false,
+  },
+
+});
